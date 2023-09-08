f78046710fcc771ff0b34d0695f1ccc8a189f2b3
5uperb0y <fanjenyan@gmail.com>
2023-09-08 19:36:59 +0800

    refactor: remove `DiffMode` for simplifying

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import argparse
 import pandas as pd
+# ARGUMENT PARSING
+def parse_arguments():
+	parser = argparse.ArgumentParser(description = "A table comparison tool, inspired by GNU diff.")
+	parser.add_argument("file1", type = str, help = "Path to the first file to compare.")
+	parser.add_argument("file2", type = str, help = "Path to the second file to compare.")
+	parser.add_argument("-i", "--index", type = str, help = "Name of index column, focusing comparison based on shared indices")
+	parser.add_argument("-c", "--column", type = str, help = "Only compare the specified columns across the two tables. If not provided, all columns will be compared.")
+	parser.add_argument("-C", "--common-columns", action = "store_true", help = "Only compare columns that exist in both tables.")
+	return parser.parse_args()
+# DATA LOADING
 def load_data(f_path, index = None, column = None):
 	df = pd.read_csv(f_path, sep = "\t", keep_default_na = False, dtype = str)
 	if index:
@@ -18,7 +28,25 @@ def row_to_header(df, row_index: int = 0):
 	df.columns = new_header
 	return df
 def header_to_row(df):
-	return pd.concat([df.columns.to_frame().T, df]).reset_index(drop = True)
+	df = pd.concat([df.columns.to_frame().T, df]).reset_index(drop = True)
+	df.columns = range(df.shape[1])
+	return df
+# PREPROCESSING
+def dehead(df1, df2):
+	df1 = header_to_row(df1)
+	df2 = header_to_row(df2)
+	return df1, df2
+def intersect_by_ids(df1, df2):
+	shared_ids = df1.index.intersection(df2.index)
+	df1 = df1.loc[shared_ids].sort_index()
+	df2 = df2.loc[shared_ids].sort_index()
+	return df1, df2
+def intersect_by_columns(df1, df2):
+	common_columns = df1.columns.intersection(df2.columns)
+	df1 = df1[common_columns]
+	df2 = df2[common_columns]
+	return df1, df2
+# COMPARING
 def compare_df(df1, df2):
 	return df1.combine(df2, compare_series)
 def compare_series(s1, s2):
@@ -30,35 +58,15 @@ def combine_cells(c1, c2):
 		return c1
 	else:
 		return c1 + "{" + c2 + "}"
-class DiffMode():
-	def compare(self, df1, df2):
-		pass
-class DefaultMode(DiffMode):
-	def compare(self, df1, df2):
-		df1 = header_to_row(df1)
-		df2 = header_to_row(df2)
-		return compare_df(df1, df2)
-class IndexMode(DiffMode):
-	def compare(self, df1, df2):
-		shared_ids = df1.index.intersection(df2.index)
-		common_df1 = header_to_row(df1.loc[shared_ids].sort_index())
-		common_df2 = header_to_row(df2.loc[shared_ids].sort_index())
-		return compare_df(common_df1, common_df2)
-def parse_arguments():
-	parser = argparse.ArgumentParser(description = "A table comparison tool, inspired by GNU diff.")
-	parser.add_argument("file1", type = str, help = "Path to the first file to compare.")
-	parser.add_argument("file2", type = str, help = "Path to the second file to compare.")
-	parser.add_argument("-i", "--index", type = str, help = "Name of index column, focusing comparison based on shared indices")
-	parser.add_argument("-c", "--column", type = str, help = "Only compare the specified columns across the two tables. If not provided, all columns will be compared.")
-	return parser.parse_args()
 def main():
 	args = parse_arguments()
 	df1 = load_data(args.file1, args.index, args.column)
 	df2 = load_data(args.file2, args.index, args.column)
+	if args.common_columns:
+		df1, df2 = intersect_by_columns(df1, df2)
 	if args.index:
-		mode = IndexMode()
-	else:
-		mode = DefaultMode()
-	diff_df = mode.compare(df1, df2)
-	print(diff_df.to_csv(sep = "\t", index = None, header = None))
+		df1, df2 = intersect_by_ids(df1, df2)
+	df1, df2 = dehead(df1, df2)
+	diff_df = compare_df(df1, df2)
+	print(diff_df.to_csv(sep = "\t", index = None, header = False))
 main()
